572f75a2ab5dcaa2fee9f32c9ff9d1a2c1f7ab93
Arunarchitect <intformbase@gmail.com>
2024-01-29 22:28:22 +0530

    new development

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ npm run build
 
 
 echo "Deploying files to server..."
-scp -r dist/* arun@139.84.140.96:/var/www/arunarchitect.in/
+scp -r dist/* ubuntu@3.92.184.234:/var/www/arunarchitect.in/
 
 echo "Done"
