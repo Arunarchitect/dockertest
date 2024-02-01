9815d5b2ec197791fc0e6deff95b0362a9118762
Arunarchitect <intformbase@gmail.com>
2024-02-01 14:03:50 +0530

    test

--- src/components/hooks/usePageSEO/index.js
+++ src/components/hooks/usePageSEO/index.js
@@ -1,48 +1,48 @@
 import { useEffect } from "react";
-
 const usePageSEO = ({
     title,
     description,
-    keywords=[],
+    keywords = [],
     ogTitle,
     ogDescription,
     ogImage,
     ogUrl
 }) => {
-    useEffect(()=> {
-        document.title  =title
-        setMetaTag('name', 'description', description)
-        setMetaTag('name', 'keywords', keywords)
-        setMetaTag('property', 'og:title', ogTitle ||title)
-        setMetaTag('property', 'description', ogDescription ||description)
-        setMetaTag('property', 'og:image', ogImage)
-        setMetaTag('property', 'og:url', ogUrl || window.location.href)
+    useEffect(() => {
+        document.title = title;
+        setMetaTag('name', 'description', description);
+        setMetaTag('name', 'keywords', keywords);
+        setMetaTag('property', 'og:title', ogTitle || title);
+        setMetaTag('property', 'description', ogDescription || description);
 
-        return () => {
-            // don any kind of cleanup  here
+        // Explicitly provide 'og:image' if available
+        if (typeof ogImage === 'string') {
+            setMetaTag('property', 'og:image', ogImage);
+        } else if (ogImage && ogImage.url) {
+            setMetaTag('property', 'og:image', ogImage.url);
         }
+
+        setMetaTag('property', 'og:url', ogUrl || window.location.href);
+
+        // Include the date and method of scraping in a comment
+        // For example: <!-- Last scraped on: 2024-02-01, Method: Manual -->
         
-    },[title,
-        description,
-        keywords=[],
-        ogTitle,
-        ogDescription,
-        ogImage,
-        ogUrl
-    ])
-    
+        return () => {
+            // Do any kind of cleanup here
+        };
+    }, [title, description, keywords, ogTitle, ogDescription, ogImage, ogUrl]);
 
     const setMetaTag = (attr, key, content) => {
-        if(content) {
-            let element = document.querySelector(`meta[${attr} ="${key}"]`)
-            if (!element){
-                element = document.createElement('meta')
-                element.setAttribute(attr, key)
-                document.head.appendChild(element)
+        if (content) {
+            let element = document.querySelector(`meta[${attr}="${key}"]`);
+            if (!element) {
+                element = document.createElement('meta');
+                element.setAttribute(attr, key);
+                document.head.appendChild(element);
             }
-            element.setAttribute('content',content)
+            element.setAttribute('content', content);
         }
-    }
-}
+    };
+};
 
 export default usePageSEO
