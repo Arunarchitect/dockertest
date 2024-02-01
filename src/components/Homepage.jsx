0ac80fc7ceaa17018cedc793e8e5faf1ca4816de
Arunarchitect <intformbase@gmail.com>
2024-02-01 13:22:47 +0530

    test helmet

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,7 +14,7 @@ const Homepage = () => {
         <meta property="og:title" content="Welcome to my website" />
         <meta property="og:description" content="All about my works" />
         <meta property="og:image" content={iImg} />
-        <meta property="og:url" content="http://arunarchitect.in" />
+        <meta property="og:url" content="https://arunarchitect.in" />
         <meta property="og:type" content="website" />
       </Helmet>
         <Hero />
