e2fb7a78426decef517b3982a720d734b55ab552
Arunarchitect <intformbase@gmail.com>
2024-02-02 11:39:24 +0530

    added ogtest from offi.  doc

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,15 +7,6 @@ import i2Img from "../images/p1.jpg";
 import usePageSEO from './hooks/usePageSEO';
 
 const Homepage = () => {
-  usePageSEO({
-    title: "PAge1",
-    description: "Page1 des",
-    keywords:["blah","jah"],
-    ogTitle: "Page 1 og",
-    ogDescription: "Page 1 ogdes ",
-    ogImage: `${i2Img}`,
-    ogUrl: "https://data",
-  })
   return (
     <>
         <Hero />
